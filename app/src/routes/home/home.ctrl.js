dcefb355fa83c466813ce94a82095c6d425e3b11
hyoje0523 <hyoje0523@gmail.com>
2024-02-11 00:21:13 +0900

    User model

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -13,22 +13,8 @@ const UserStorage = require("../../models/UserStorage");
 
 const process = {
     login: (req, res) => {  
-        const id = req.body.id,
-            password = req.body.password;
-
-        const users = UserStorage.getUsers("id", "password");
-
-        const response = {};
-        if(users.id.includes(id)) {
-            const idx = users.id.indexOf(id);
-            if(users.password[idx] === password) {
-                response.success = true;
-                return res.json(response);
-            }
-        }
-
-        response.success = false;
-        response.msg = "login fail";
+        const user = new User(req.body);
+        const response = user.login();
         return res.json(response);
     },
 };
